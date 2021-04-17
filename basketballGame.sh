a2604b3d6b86aae6b6963c1bd2c0def1c58951c0
cthacker-udel <cthacker@udel>
2021-04-17 00:45:23 -0400

    Refined game, added print statements displaying the score, and also added if statement at end displaying which team won

--- basketballGame.sh
+++ basketballGame.sh
@@ -27,9 +27,10 @@ else
 fi
 
 while [ "$loopVar" -gt 0 ]; do
+    echo -e "\n\n---------------\nSCORE : YOU $yourScore \t CPU $computerScore\n"
     if [ $posession -eq 1 ]; then
         # you have possesion
-        read -p "What type of shot will it be? 1) Layup 2) Dunk 3) Two-Point Jumpshot 4) Three-Pointer Jumpshot" choice
+        read -p "What type of shot will it be? 1) Layup 2) Dunk 3) Two-Point Jumpshot 4) Three-Pointer Jumpshot   " choice
         if [ $choice -lt 1 -o $choice -gt 4 ]; then
             echo -e "Not a valid choice, switching possesion"
             posession=2
@@ -41,20 +42,24 @@ while [ "$loopVar" -gt 0 ]; do
                 if [ $computerRandomVal -eq 1 ]; then
                     #blocked
                     echo -e "\nYour layup has been blocked, switching posession\n"
-                    possesion=2
+                    sleep 1
+                    posession=2
                     loopVar=$((loopVar - 5))
                     continue
                 else
                     echo -e "\nYour layup was not blocked\n"
+                    sleep 1
                     yourRandomVal=$(shuf -i 1-2 -n 1)
                     if [ $yourRandomVal -eq 1 ]; then
                         echo -e "\nShot successful, 2 points made\n"
+                        sleep 1
                         yourScore=$((yourScore + 2))
                         loopVar=$((loopVar - 5)) 
                         posession=2
                         continue
                     else
                         echo -e "Layup missed"
+                        sleep 1
                         loopVar=$((loopVar - 5)) 
                         posession=2
                         continue
@@ -65,20 +70,24 @@ while [ "$loopVar" -gt 0 ]; do
                 computerRandomVal=$(shuf -i 1-2 -n 1)
                 if [ $computeRandomVal -eq 1 ]; then
                     echo -e "\nYour dunk has been blocked, switching possesion\n"
+                    sleep 1
                     posesion=2
                     loopVar=$((loopVar - 5)) 
                     continue
                 else
                     echo -e "\nYour dunk was not blocked\n"
+                    sleep 1
                     yourRandomVal=$(shuf -i 1-2 -n 1)
                     if [ $yourRandomVal -eq 1 ]; then
                         echo -e "\nDunk successful, 2 points made\n"
+                        sleep 1
                         yourScore=$((yourScore + 2))
                         loopVar=$((loopVar - 5)) 
                         posession=2
                         continue
                     else
                         echo -e "Dunk missed"
+                        sleep 1
                         loopVar=$((loopVar - 5)) 
                         posession=2
                         continue
@@ -89,20 +98,24 @@ while [ "$loopVar" -gt 0 ]; do
                computerRandomVal=$(shuf -i 1-3 -n 1)
                if [ $computerRandomVal -eq 1 ]; then
                    echo -e "Your 2 points jumpshot has been blocked, switching possesion\n"
+                   sleep 1
                    posession=2
                    loopVar=$((loopVar - 5)) 
                    continue
                else
                    echo -e "\nYour 2 point jumpshot has not been blocked\n"
+                   sleep 1
                    yourRandomVal=$(shuf -i 1-2 -n 1)
                    if [ $yourRandomVal -eq 1 ]; then
                        echo -e "\nYour 2 pointer was successful, 2 points made\n"
+                       sleep 1
                        yourScore=$((yourScore+2))
                        loopVar=$((loopVar - 5)) 
                        posession=2
                        continue
                    else
                        echo -e "\nYour 2 point jumpshot was unsuccessful\n"
+                       sleep 1
                        loopVar=$((loopVar - 5)) 
                        posession=2
                        continue
@@ -113,19 +126,24 @@ while [ "$loopVar" -gt 0 ]; do
               computerRandomVal=$(shuf -i 1-4 -n 1)
               if [ $computerRandomVal -eq 1 ]; then
                   echo "Your 3 point shot has been blocked"
+                  sleep 1
                   posession=2
                   loopVar=$((loopVar - 5)) 
                   continue
               else
                   echo -e "\nYour 3 point shot has not been blocked\n"
+                  sleep 1
                   yourRandomVal=$(shuf -i 1-2 -n 1)
                   if [ $yourRandomVal -eq 1 ]; then
                       echo -e "\n Your 3 pointer was successful, 3 points made\n"
+                      sleep 1
+                      yourScore=$((yourScore + 3))
                       posession=2
                       loopVar=$((loopVar - 5)) 
                       continue
                   else
                       echo -e "\nYour 3 pointer was unsuccessful\n"
+                      sleep 1
                       loopVar=$((loopVar - 5)) 
                       posession=2
                       continue
@@ -138,14 +156,17 @@ while [ "$loopVar" -gt 0 ]; do
         computerShotChoice=$(shuf -i 1-4 -n 1)
         if [ $computerShotChoice -eq 1 ]; then
             echo "Layup chosen"
+            sleep 1
             yourRandomVal=$(shuf -i 1-2 -n 1)
             if [ "$yourRandomVal" -eq 1 ]; then
                 echo "You blocked the computers shot!"
+                sleep 1
                 posession=1
                 loopVar=$((loopVar - 5))
                 continue
             else
                 echo "Computer made layup, 2 points"
+                sleep 1
                 posession=1
                 computerScore=$((computerScore + 2))
                 loopVar=$((loopVar - 5))
@@ -153,14 +174,17 @@ while [ "$loopVar" -gt 0 ]; do
             fi
         elif [ $computerShotChoice -eq 2 ]; then
             echo "Dunk chosen"
+            sleep 1
             yourRandomVal=$(shuf -i 1-2 -n 1)
             if [ $yourRandomVal -eq 1 ]; then
                 echo "You blocked the computer's dunk"
+                sleep 1
                 posession=1
                 loopVar=$((loopVar-5))
                 continue
             else
                 echo "Computer dunks"
+                sleep 1
                 posession=1
                 computerScore=$((computerScore + 2))
                 loopVar=$((loopVar-5))
@@ -168,14 +192,17 @@ while [ "$loopVar" -gt 0 ]; do
             fi
         elif [ $computerShotChoice -eq 3 ]; then
             echo "Two pointer chosen"
+            sleep 1
             yourRandomVal=$(shuf -i 1-3 -n 1)
             if [ $yourRandomVal -eq 1 ]; then
                 echo "You blocked the compute's two point jumpshot"
+                sleep 1
                 posession=1
                 loopVar=$((loopVar-5))
                 continue
             else
                 echo "Computer made two point jumpshot"
+                sleep 1
                 posession=1
                 computerScore=$((computerScore + 2))
                 loopVar=$((loopVar-5))
@@ -183,14 +210,17 @@ while [ "$loopVar" -gt 0 ]; do
             fi
         elif [ $computerShotChoice -eq 4 ]; then
             echo "Three pointer chosen"
+            sleep 1
             yourRandomVal=$(shuf -i 1-4 -n 1)
             if [ $yourRandomVal -eq 1 ]; then
                 echo "You blocked the computer's three point jump shot"
+                sleep 1
                 posession=1
                 loopVar=$((loopVar-5))
                 continue
             else
                 echo "Computer made three point jumpshot"
+                sleep 1
                 posession=1
                 computerScore=$((computerScore + 3))
                 loopVar=$((loopVar-5))
@@ -200,6 +230,13 @@ while [ "$loopVar" -gt 0 ]; do
 
     fi
 done
+if [ "$yourScore" -gt "$computerScore" ]; then
+    echo "Your team wins!"
+elif [ "$computerScore" -gt "$yourScore" ]; then
+    echo "Computer's team wins!"
+else
+    echo "Tie!"
+fi
 
 
 
